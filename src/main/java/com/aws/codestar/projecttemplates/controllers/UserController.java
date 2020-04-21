e29c9bf9b9f80192075956278866d0f78552f56e
Gabriel Nicolae <gabriel.nicolae313@gmail.com>
2020-04-21 16:52:33 +0300

    Find User by login

--- src/main/java/com/aws/codestar/projecttemplates/controllers/UserController.java
+++ src/main/java/com/aws/codestar/projecttemplates/controllers/UserController.java
@@ -3,9 +3,11 @@ package com.aws.codestar.projecttemplates.controllers;
 import com.aws.codestar.projecttemplates.models.User;
 import com.aws.codestar.projecttemplates.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/users")
@@ -21,5 +23,13 @@ public class UserController {
     @PostMapping
     User newUser(@RequestBody User user) {
         return userRepository.save(user);
+    }
+
+    @GetMapping("/login/{login}")
+    ResponseEntity<User> findUserByLogin(@PathVariable(value = "login") String login) {
+        Optional<User> user = userRepository.findUserByLogin(login);
+        if(!user.isPresent())
+            return ResponseEntity.notFound().build();
+        return ResponseEntity.ok(user.get());
     }
 }
