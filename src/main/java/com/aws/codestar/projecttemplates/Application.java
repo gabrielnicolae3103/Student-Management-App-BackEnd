49e46fe5f84ec69dd5ed267798599e3f763a131e
Gabriel Nicolae <gabriel.nicolae313@gmail.com>
2020-04-25 22:42:47 +0300

    cors

--- src/main/java/com/aws/codestar/projecttemplates/Application.java
+++ src/main/java/com/aws/codestar/projecttemplates/Application.java
@@ -4,14 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-/** Simple class to start up the application.
- *
- * @SpringBootApplication adds:
- *  @Configuration
- *  @EnableAutoConfiguration
- *  @ComponentScan
- */
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
 
@@ -22,6 +18,22 @@ public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "OPTIONS", "PUT")
+                        .allowedHeaders("Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
+                                "Access-Control-Request-Headers")
+                        .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials")
+                        .allowCredentials(true).maxAge(3600);
+            }
+        };
     }
 
 }
